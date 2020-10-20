3764db58982751e7ec71f0645f229d71717a78d8
utsavpatel22 <erutsavpatel22@gmail.com>
2020-10-20 17:22:42 -0400

    [linear_classifier]: Finished train function of the linear classifier

--- assignment1/cs231n/classifiers/linear_classifier.py
+++ assignment1/cs231n/classifiers/linear_classifier.py
@@ -57,7 +57,9 @@ class LinearClassifier(object):
             #########################################################################
             # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-            pass
+            mask = np.random.choice(num_train, batch_size, replace=True)
+            X_batch = X[mask]
+            y_batch = y[mask]
 
             # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -72,7 +74,7 @@ class LinearClassifier(object):
             #########################################################################
             # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-            pass
+            self.W += - learning_rate * grad
 
             # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
